0a51f9da860781474a13804f6f7a20909c9023d4
Kunal Singh <kunalsingh.iitkgp@gmail.com>
2017-04-03 18:01:19 +0530

    Update train_cnn.py

--- MNISTClassification_using_tensorflow/train_cnn.py
+++ MNISTClassification_using_tensorflow/train_cnn.py
@@ -1,50 +1,152 @@
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import optimizers
-import numpy as np
-from keras.models import load_model
+'''
+Deep Learning Programming Assignment 2
+--------------------------------------
+Name:
+Roll No.:
 
+======================================
+Complete the functions in this file.
+Note: Do not change the function signatures of the train
+and test functions
+'''
+import numpy as np
+import tensorflow as tf
+
+def conv2d(x, W, b, strides=1):
+    # Conv2D wrapper, with bias and relu activation
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+
+def maxpool2d(x, k=2):
+    # MaxPool2D wrapper
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
+                          padding='SAME')
+
+
+# Create model
+def conv_net(x, weights, biases, dropout):
+    # Reshape input picture
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+
+    # Convolution Layer
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    # Max Pooling (down-sampling)
+    conv1 = maxpool2d(conv1, k=2)
+
+    # Convolution Layer
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    # Max Pooling (down-sampling)
+    conv2 = maxpool2d(conv2, k=2)
+
+    # Fully connected layer
+    # Reshape conv2 output to fit fully connected layer input
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    # Apply Dropout
+    fc1 = tf.nn.dropout(fc1, dropout)
+
+    # Output, class prediction
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return out
 
 
 def train(trainX, trainY):
     '''
     Complete this function.
     '''
+    trainX = trainX.reshape(-1,784)
     trainY = trainY.reshape(60000,1)
-
+    
 
     tr_Y = np.zeros((60000, 10))
     for i in range(tr_Y.shape[0]):
         tr_Y[i,trainY[i]] = 1
 
-    model = Sequential()
-    # input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
-    # this applies 32 convolution filters of size 3x3 each.
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.1))
+    '''te_Y = np.zeros((10000, 10))
+    for i in range(te_Y.shape[0]):
+        te_Y[i,testY[i]] = 1'''
+    
+    learning_rate = 0.006
+    training_epochs = 60
+    batch_size = 100
+    display_step = 1
 
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.1))
+    # Network Parameters
+    n_input = 784 # MNIST data input (img shape: 28*28)
+    n_classes = 10 # MNIST total classes (0-9 digits)
+    dropout = 0.75 # Dropout, probability to keep units
 
-    model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.1))
-    model.add(Dense(10, activation='softmax'))
+    # tf Graph input
+    x = tf.placeholder(tf.float32, [None, n_input])
+    y = tf.placeholder(tf.float32, [None, n_classes])
+    keep_prob = tf.placeholder(tf.float32)
 
-    Adam = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-    model.compile(loss='categorical_crossentropy', optimizer=Adam)
+    weights = {
+        # 5x5 conv, 1 input, 32 outputs
+        'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+        # 5x5 conv, 32 inputs, 64 outputs
+        'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+        # fully connected, 7*7*64 inputs, 1024 outputs
+        'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+        # 1024 inputs, 10 outputs (class prediction)
+        'out': tf.Variable(tf.random_normal([1024, n_classes]))
+    }
 
-    model.fit(trainX, tr_Y, batch_size=256, epochs=35)
+    biases = {
+        'bc1': tf.Variable(tf.random_normal([32])),
+        'bc2': tf.Variable(tf.random_normal([64])),
+        'bd1': tf.Variable(tf.random_normal([1024])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
 
-    model.save('weights/cnn.h5')
+    # Construct model
+    pred = conv_net(x, weights, biases, keep_prob)
+
+    # Define loss and optimizer
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # Evaluate model
+    saver = tf.train.Saver()
+
+
+    # Initializing the variables
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(training_epochs):
+            avg_cost = 0.0
+            total_batch = int(trainX.shape[0]/batch_size)
+            # Loop over all batches
+            for i in range(total_batch):
+                batch_x, batch_y = trainX[i*batch_size:(i+1)*batch_size,:], tr_Y[i*batch_size:(i+1)*batch_size,:]    
+                # Run optimization op (backprop) and cost op (to get loss value)
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                              y: batch_y, keep_prob: dropout})
+                # Compute average loss
+                avg_cost += c / total_batch
+            # Display logs per epoch step
+            if epoch % display_step == 0:
+                print("Epoch:", '%04d' % (epoch+1), "cost=", \
+                    "{:.9f}".format(avg_cost))
+        print("Optimization Finished!")
+
+        # Calculate accuracy for 256 mnist test images
+
+        
+    
+        
+
+
+        #savepath = saver.save(sess,'model_cnn.ckpt')
 
 
 
-def test(testX):
+def test(testX,testY):
     '''
     Complete this function.
     This function must read the weight files and
@@ -54,6 +156,63 @@ def test(testX):
     of the array should contain the label of the i-th test
     example.
     '''
-    model = load_model('weights/cnn.h5')
+    testX = testX.reshape(-1,784)
+    testY = testY.reshape(10000,1)
 
-    return np.argmax(model.predict(testX),1)
+    te_Y = np.zeros((10000, 10))
+    for i in range(te_Y.shape[0]):
+        te_Y[i,testY[i]] = 1
+
+    # Network Parameters
+    n_input = 784 # MNIST data input (img shape: 28*28)
+    n_classes = 10 # MNIST total classes (0-9 digits)
+    dropout = 0.75 # Dropout, probability to keep units
+
+    # tf Graph input
+    x = tf.placeholder(tf.float32, [None, n_input])
+    y = tf.placeholder(tf.float32, [None, n_classes])
+    keep_prob = tf.placeholder(tf.float32)
+
+    weights = {
+        # 5x5 conv, 1 input, 32 outputs
+        'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+        # 5x5 conv, 32 inputs, 64 outputs
+        'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+        # fully connected, 7*7*64 inputs, 1024 outputs
+        'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+        # 1024 inputs, 10 outputs (class prediction)
+        'out': tf.Variable(tf.random_normal([1024, n_classes]))
+    }
+
+    biases = {
+        'bc1': tf.Variable(tf.random_normal([32])),
+        'bc2': tf.Variable(tf.random_normal([64])),
+        'bd1': tf.Variable(tf.random_normal([1024])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
+
+    # Construct model
+    pred = conv_net(x, weights, biases, keep_prob)
+
+    # Evaluate model
+    saver = tf.train.Saver()
+
+    # Initializing the variables
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess1:
+        sess1.run(tf.global_variables_initializer())
+        saver.restore(sess1, "/home/kunal/DLcourse/third ass/cnn/model_cnn.ckpt")
+        correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        #for i in range(10):
+        #   print("Accuracy:", accuracy.eval({x: testX[i*1000:(i+1)*1000,], y: te_Y[i*1000:(i+1)*1000,], keep_prob: 1.0}))
+
+        label = np.zeros((10000))
+        for i in range(10):
+            label_p = pred.eval(feed_dict = {x: testX[i*1000:(i+1)*1000,], keep_prob: 1.0})
+            print label_p.shape
+
+            label[i*1000:(i+1)*1000] = np.argmax(label_p,1)
+
+    return label
